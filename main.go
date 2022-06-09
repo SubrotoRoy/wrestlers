70f0d3d5683f5960296b40770d13b3ae1c9368aa
Subroto Roy <subroto.roy@ibm.com>
2022-06-09 15:50:49 +0530

    Some awesome title of about 50 chars
    Issue:
    
    A description that is wrapped about 72 chars so shows up
    decently in `git log`.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	log.Println("Strarting Wrestlers server")
 
+	log.Println("Strarting Wrestlers server")
 	http.HandleFunc("/get", handlers.GetAllWrestlers)
 	http.HandleFunc("/create", handlers.CreateWrestler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
